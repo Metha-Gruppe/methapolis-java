4ddb6d5f3c045dd58424a7896491303dd212bf55
egal27 <yens.schmied@gmail.com>
2014-03-13 16:36:45 +0100

    dirty splash

--- src/micropolisj/Main.java
+++ src/micropolisj/Main.java
@@ -8,7 +8,16 @@
 
 package micropolisj;
 
-import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 import micropolisj.gui.MainWindow;
 
@@ -23,9 +32,51 @@ public class Main
 
 	public static void main(String [] args)
 	{
+		try {
+			final BufferedImage image;
+			image = ImageIO.read(new File("graphics/airport.png"));
+			
+			JFrame frame = buildFrame(image.getWidth(), image.getHeight());
+
+
+        JPanel pane = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(image, 0, 0, null);
+            }
+        };
+
+
+        frame.add(pane);
+		
+		
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        frame.setVisible(false);
+        frame.dispose();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		SwingUtilities.invokeLater(new Runnable() {
 		public void run() {
 			createAndShowGUI();
 		}});
+	}	
+	
+	
+	private static JFrame buildFrame(int x, int y) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setSize(x, y);
+		frame.setVisible(true);
+		return frame;
 	}
+
+			
+			
 }
