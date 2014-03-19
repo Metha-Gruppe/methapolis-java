41e3e13c430459846e5b18f6f6c5733c49e44e72
Jim Neuendorf <neuendorf@companion.de>
2014-03-19 15:51:56 +0100

    made researchtree a singleton

--- src/micropolisj/research/ResearchState.java
+++ src/micropolisj/research/ResearchState.java
@@ -11,125 +11,137 @@ import java.util.*;
 import javax.swing.*;
 
 public class ResearchState extends JFrame implements ActionListener {
-	
+	private static final long serialVersionUID = 1L;
+
 	int rocket_speed = 1;
-	
+
 	ResearchTree tree = new ResearchTree();
-	
+
 	HashSet<Integer> reached_nodes = new HashSet<Integer>();
-	
+
 	int researchPoints = 20;
-	
+
 	int buttonWidth = 150;
 	int buttonHeight = 75;
 	int maxWidth = 0;
 	int maxHeight = 0;
-	
+
 	JScrollPane scrollPane;
 	JPanel insidePane;
 	JPanel subMenu;
 	JLabel researchPointsLabel;
 	JButton closeButton;
-	JButton [] buttons;
+	JButton[] buttons;
+	
+	// SINGLETON PATTERN
+	private static ResearchState instance = null;
+	public ResearchState getInstance()	{
+		if(instance == null)	{
+			return new ResearchState();
+		}
+		else	{
+			return instance;
+		}
+	}
 
-	public ResearchState(){
+	public ResearchState() {
 		setName("Research Tree");
 		setLayout(new BorderLayout());
-		
+
 		insidePane = new JPanel();
 		insidePane.setLayout(null);
-		
+
 		scrollPane = new JScrollPane(insidePane);
-		//scrollPane.setLayout(null);
+		// scrollPane.setLayout(null);
 		add(scrollPane, BorderLayout.CENTER);
-		
-		
-		
+
 		subMenu = new JPanel();
 		subMenu.setLayout(new BorderLayout());
 		add(subMenu, BorderLayout.SOUTH);
-		
+
 		researchPointsLabel = new JLabel(Integer.toString(researchPoints) + " research points");
 		subMenu.add(researchPointsLabel, BorderLayout.EAST);
-		
+
 		closeButton = new JButton("close");
-		closeButton.addActionListener(new ActionListener(){
+		closeButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				setVisible(false);
 			}
 		});
 		subMenu.add(closeButton);
-		
-		
-		
-		int n = tree.possible_nodes.length;
-		buttons = new JButton[n];
-		
-		for(int node_id=0;node_id < n;node_id++){
-			ResearchNode node = tree.possible_nodes[node_id];
-			
-			buttons[node_id] = new JButton("<html>"+node.getDesc()+"</html>");
-			buttons[node_id].setBounds(new Rectangle(tree.positionsX[node_id], tree.positionsY[node_id], buttonWidth, buttonHeight));
 
-			maxWidth = Math.max(maxWidth, tree.positionsX[node_id] + buttonWidth);
-			maxHeight = Math.max(maxHeight, tree.positionsY[node_id] + buttonHeight);
-			
+		int n = ResearchTree.possible_nodes.length;
+		buttons = new JButton[n];
+
+		for(int node_id = 0; node_id < n; node_id++) {
+			ResearchNode node = ResearchTree.possible_nodes[node_id];
+
+			buttons[node_id] = new JButton("<html>" + node.getDesc() + "</html>");
+			buttons[node_id].setBounds(new Rectangle(ResearchTree.positionsX[node_id], ResearchTree.positionsY[node_id],
+					buttonWidth, buttonHeight));
+
+			maxWidth = Math.max(maxWidth, ResearchTree.positionsX[node_id] + buttonWidth);
+			maxHeight = Math.max(maxHeight, ResearchTree.positionsY[node_id] + buttonHeight);
+
 			insidePane.add(buttons[node_id]);
 		}
-		
+
 		insidePane.setPreferredSize(new Dimension(maxWidth, maxHeight));
-		
+
 		refreshPanel();
-		
+
 		setVisible(false);
 	}
-	
-	public void showResearchPanel(){
+
+	public void showResearchPanel() {
 		setBounds(200, 200, maxWidth + 10, maxHeight + 100);
-		
+
 		setVisible(true);
 	}
-	
-	private void refreshPanel(){
-		
+
+	private void refreshPanel() {
+
 		researchPointsLabel.setText(Integer.toString(researchPoints) + " research points");
-		
-		for(int node_id=0;node_id < tree.possible_nodes.length;node_id++){
-			ResearchNode node = tree.possible_nodes[node_id];
-			
-			if(reached_nodes.contains(node_id)){
+
+		for(int node_id = 0; node_id < ResearchTree.possible_nodes.length; node_id++) {
+			ResearchNode node = ResearchTree.possible_nodes[node_id];
+
+			if(reached_nodes.contains(node_id)) {
 				buttons[node_id].setEnabled(false);
 				buttons[node_id].setBackground(Color.red);
-			}else if(!tree.isReachable(reached_nodes, node_id)){
+			}
+			else if(!tree.isReachable(reached_nodes, node_id)) {
 				buttons[node_id].setEnabled(false);
-			}else{
+			}
+			else {
 				System.out.println(node_id);
-				
+
 				buttons[node_id].setActionCommand(Integer.toString(node_id));
 				buttons[node_id].setEnabled(true);
 				buttons[node_id].addActionListener(this);
 			}
 		}
-		
+
 		revalidate();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent ev) {
 		int node_id = Integer.parseInt(ev.getActionCommand());
-		ResearchNode node = tree.possible_nodes[node_id];
+		ResearchNode node = ResearchTree.possible_nodes[node_id];
 		int cost = node.getCost();
-		
-		if(researchPoints >= cost){
-			
+
+		if(researchPoints >= cost) {
+
 			reached_nodes.add(node_id);
 			node.makeChanges(this);
-			
+
 			researchPoints -= cost;
-			
+
 			refreshPanel();
-		}else{
+		}
+		else {
 			JOptionPane.showMessageDialog(this, "You don't have enough research points for this upgrade.");
 		}
 	}
--- src/micropolisj/research/ResearchTree.java
+++ src/micropolisj/research/ResearchTree.java
@@ -1,46 +1,58 @@
 package micropolisj.research;
 
-import java.awt.GridLayout;
-import java.awt.Rectangle;
 import java.util.HashSet;
-
-import javax.swing.*;
-
-import micropolisj.research.ResearchNode;
 
 public class ResearchTree {
 
 	static ResearchNode[] possible_nodes;// = {new RocketRangeNode()};
-	static int [][] needed = {{}, {0}};
+	static int[][] needed = {
+			{}, {
+				0
+			}
+	};
 	int height = 2, width = 2;
-	static int [] positionsX = {0, 120};
-	static int [] positionsY = {0, 70};
-	
-	ResearchTree(){
-		possible_nodes = new ResearchNode[]{new RocketRangeNode(), new RocketDamageNode()};
+	static int[] positionsX = {
+			0, 120
+	};
+	static int[] positionsY = {
+			0, 70
+	};
+
+	ResearchTree() {
+		possible_nodes = new ResearchNode[] {
+				new RocketRangeNode(), new RocketDamageNode()
+		};
 	}
-	
-	boolean isReachable(HashSet<Integer> reached_nodes, int node_id){
-		for(int nid : needed[node_id]){
+
+	boolean isReachable(HashSet<Integer> reached_nodes, int node_id) {
+		for(int nid : needed[node_id]) {
 			if(!reached_nodes.contains(nid))
 				return false;
 		}
 		return true;
-			
+
 	}
 
+	// INNER CLASSES FOR UPGRADE FEATURES
 	class RocketRangeNode implements ResearchNode {
 
 		@Override
-		public String getDesc() {return "increase rockets range";}
+		public String getDesc() {
+			return "increase rockets range";
+		}
 
 		@Override
-		public String getIcon() {return "/firestation.png";}
-		
+		public String getIcon() {
+			return "/firestation.png";
+		}
+
 		@Override
-		public int getCost() {return 15;}
-		//@Override
-		//public int getId(){return 0;}
+		public int getCost() {
+			return 15;
+		}
+
+		// @Override
+		// public int getId(){return 0;}
 
 		@Override
 		public void makeChanges(ResearchState state) {
@@ -51,15 +63,22 @@ public class ResearchTree {
 	class RocketDamageNode implements ResearchNode {
 
 		@Override
-		public String getDesc() {return "increase rockets damage";}
+		public String getDesc() {
+			return "increase rockets damage";
+		}
 
 		@Override
-		public String getIcon() {return "/coal.png";}
+		public String getIcon() {
+			return "/coal.png";
+		}
 
 		@Override
-		public int getCost() {return 20;}
-		//@Override
-		//public int getId(){return 0;}
+		public int getCost() {
+			return 20;
+		}
+
+		// @Override
+		// public int getId(){return 0;}
 
 		@Override
 		public void makeChanges(ResearchState state) {
